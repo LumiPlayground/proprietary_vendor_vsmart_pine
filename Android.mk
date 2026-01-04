759a73f86d3bbf3ef2249393e8ec18b58dd4f37e
Tuan Anh <tuan73176@gmail.com>
2026-01-04 15:59:24 +0000

    V420A_open-user 10 QKQ1.200311.002 V420A_OPN_U_B14_210622 release-keys
    Change-Id: Ia5def0c48d82ee02e649b5d2215eddf8c33239f7

---
+++ Android.mk
@@ -0,0 +1,23 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),pine)
+
+$(call add-radio-file-sha1-checked,radio/aboot.mbn,8f8504365c5b628bd94fa85f6d081927ad60b037)
+$(call add-radio-file-sha1-checked,radio/cmnlib.mbn,1aea71535f15a10009b339346bf14070ceec73cd)
+$(call add-radio-file-sha1-checked,radio/cmnlib64.mbn,9df26a1155d7c6c199c94277db618ffa77fe42a2)
+$(call add-radio-file-sha1-checked,radio/devcfg.mbn,c43eadfe54be0938d7517c64bcf033b32b9930c7)
+$(call add-radio-file-sha1-checked,radio/dsp.bin,67c7b54b49cb21e6d9b08d037c2b7c68b9ce65e7)
+$(call add-radio-file-sha1-checked,radio/keymaster.mbn,c690c036670f0013a659b50b1cf2788d6fe1b1a6)
+$(call add-radio-file-sha1-checked,radio/lksecapp.mbn,23077b5fe1b3c5b92214c1a80381501820ac57dd)
+$(call add-radio-file-sha1-checked,radio/mdtp.img,41e15187c7cb4e8b756b2e08f811d0e1f40a41c6)
+$(call add-radio-file-sha1-checked,radio/modem.bin,9027b24a99c9118c94503929691c449fb2c8dfa9)
+$(call add-radio-file-sha1-checked,radio/rpm.mbn,bbcde8c3f270d64438c1c9f3e91be119b96fe735)
+$(call add-radio-file-sha1-checked,radio/sbl1.mbn,54ed09bb7b1cdeab479948bdac73d163765a5081)
+$(call add-radio-file-sha1-checked,radio/splash.img,ec17c53f0236b1a2721681c88773c56aa6ab7732)
+$(call add-radio-file-sha1-checked,radio/tz.mbn,1f18846e778cd8ebd4a770964e685277eafbe196)
+
+endif
